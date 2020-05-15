356a444eb0b8a408ffdb5a1db35158da73f64a3b
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-15 05:31:31 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  until position_taken?(board, location) == location[""] && location[" "]
+  until position_taken?(board, location) != location[""] && location[" "]
   turn(board)
 end
 end
